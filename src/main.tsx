30cd305170d9dbbb7bacbf1846e8b5d25d14c498
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-04 07:23:30 +0700

    ✨ chore: import geist font same with shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import '@fontsource/geist-sans/index.css';
+import '@fontsource/geist-sans/400.css'; // Regular
+import '@fontsource/geist-sans/500.css'; // Medium
+import '@fontsource/geist-sans/600.css'; // Semi Bold
+import '@fontsource/geist-sans/700.css'; // Bold
+import '@fontsource/geist-sans/800.css'; // Extra Bold
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

fa17cf7ab2bddc1bb2eeb29a9e28373a6239607c
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-04 08:02:52 +0700

    🔥 feat: add new pages for landing n initial setoran dashboard

---
+++ src/pages/publics/landing.page.tsx
@@ -0,0 +1,11 @@
+import HeaderComponent from "@/components/globals/headers/header";
+
+const LandingPage = () => {
+	return (
+		<div className="w-screen h-screen">
+			<HeaderComponent />
+		</div>
+	);
+};
+
+export default LandingPage;

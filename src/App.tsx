e07b8fb56ca4f6ebc6a320c2dc81a96a712637ff
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-04 07:24:30 +0700

    ✨ chore: embed to authprovider oidc component, qc, n themeprovider by shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-const App = () => {
-  return (
-    <>
-      <h1 className="text-3xl text-red-700">Hei BRo</h1>
-    </>
-  )
-}
+import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "react-oidc-context";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { onSigninCallback, userManager } from "./libs/keycloak";
+import { queryClient } from "./libs/query-client";
+import { ThemeProvider } from "@/components/themes/theme-provider";
+import router from "./routers/app.router";
 
-export default App
+const App = () => {
+	return (
+		<AuthProvider userManager={userManager} onSigninCallback={onSigninCallback}>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+					<RouterProvider router={router} />
+				</ThemeProvider>
+			</QueryClientProvider>
+		</AuthProvider>
+	);
+};
+
+export default App;

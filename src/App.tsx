eed541bc8931cfd0b239e8d8e1ff104b84e3776d
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-07 23:43:05 +0700

    ⚙ chore: affected by preffered libs conf changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "react-oidc-context";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { onSigninCallback, userManager } from "./libs/keycloak";
-import { queryClient } from "./libs/query-client";
+import { onSigninCallback, userManager } from "./lib/keycloak";
+import { queryClient } from "./lib/query-client";
 import { ThemeProvider } from "@/components/themes/theme-provider";
 import router from "./routers/app.router";
 

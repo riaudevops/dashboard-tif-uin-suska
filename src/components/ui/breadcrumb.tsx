f279c1adb8ca45c04c4bc35e17d3e21ebf19b9d8
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-07 23:57:59 +0700

    🛠 fix: refactor shadcn default component breadcrumb separator by removing <li> for avoiding warn console

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -77,14 +77,14 @@ const BreadcrumbSeparator = ({
   className,
   ...props
 }: React.ComponentProps<"li">) => (
-  <li
+  <span
     role="presentation"
     aria-hidden="true"
     className={cn("[&>svg]:w-3.5 [&>svg]:h-3.5", className)}
     {...props}
   >
     {children ?? <ChevronRight />}
-  </li>
+  </span>
 )
 BreadcrumbSeparator.displayName = "BreadcrumbSeparator"
 

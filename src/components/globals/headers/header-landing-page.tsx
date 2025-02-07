48c2efe530acd470dc5812d6a8c2b022820a6c15
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-07 23:41:38 +0700

    ⚙ chore: removing dashboard header n convert to landing-page-header

--- src/components/globals/headers/header-landing-page.tsx
+++ src/components/globals/headers/header-landing-page.tsx
@@ -1,31 +1,63 @@
 import DarkLogoUSR from "@/assets/svgs/dark-logo-usr";
+import LightLogoUSR from "@/assets/svgs/light-logo-usr";
 import { ModeToggle } from "../../themes/mode-toggle";
 import { Button } from "@/components/ui/button";
-import { LucideLogIn } from "lucide-react";
+import { ArrowUpRight, LucideLogIn } from "lucide-react";
 import { HeaderLandingPageProps } from "@/interfaces/components/globals/headers/header-landing-page.interface";
+import { useTheme } from "@/components/themes/theme-provider";
+import { NavMenu } from "./nav-menu";
+import { NavMenuItems } from "@/constants/components/globals/headers/nav-menu-item.constant";
 
-const HeaderLandingPageComponent = ({onContinueWithKeycloakClicked, isAuthenticated}: HeaderLandingPageProps) => {
+const HeaderLandingPageComponent = ({
+	onContinueWithKeycloakClicked,
+	isAuthenticated,
+}: HeaderLandingPageProps) => {
+	const { theme } = useTheme();
 	return (
 		<header className="bg-background flex h-12 w-full items-center justify-between border-b pl-2 pr-4 py-2">
 			{/* Header Logo + Nav Menu */}
-			<div onClick={() => location.href = "/"} className="flex gap-4 justify-center items-center">
+			<div
+				onClick={() => (location.href = "/")}
+				className="flex gap-4 justify-center items-center"
+			>
 				{/* Header Logo */}
 				<div className="flex items-center gap-1.5 hover:bg-secondary hover:text-secondary-foreground cursor-pointer hover:scale-95 px-2 duration-300 rounded-xl active:scale-105">
-					<DarkLogoUSR className="h-8 w-8" />
-					<span className="text-base font-bold">
+					{theme === "dark" ? (
+						<DarkLogoUSR className="h-8 w-8" />
+					) : (
+						<LightLogoUSR className="h-8 w-8" />
+					)}
+					<span className="text-base font-semibold">
 						dashboard<span className="italic font-medium">.tif-usr</span>
 					</span>
 				</div>
 			</div>
 
-            <div className="flex gap-2 justify-center items-center">
-                <Button onClick={onContinueWithKeycloakClicked} size={"sm"} variant={isAuthenticated ? "destructive" : "outline"}>
-                    <LucideLogIn className="h-4 w-4" />
-                    {isAuthenticated ? "Logout" : "Continue With Keycloak"}
-                </Button>
-                {/* Theme Toggle */}
-                <ModeToggle />
-            </div>
+			<div className="flex gap-5 justify-center items-center">
+				<NavMenu navMenu={NavMenuItems.LandingPage} />
+				<div className="flex gap-2 justify-center items-center">
+					{isAuthenticated && (
+						<Button
+							size={"sm"}
+							variant={"outline"}
+							onClick={() => (location.href = "/dashboard")}
+						>
+							<ArrowUpRight className="h-4 w-4" />
+							Pergi Ke Dashboard
+						</Button>
+					)}
+					<Button
+						onClick={onContinueWithKeycloakClicked}
+						size={"sm"}
+						variant={isAuthenticated ? "destructive" : "outline"}
+					>
+						<LucideLogIn className="h-4 w-4" />
+						{isAuthenticated ? "Logout" : "Mulai Sekarang"}
+					</Button>
+					{/* Theme Toggle */}
+					<ModeToggle />
+				</div>
+			</div>
 		</header>
 	);
 };
--- src/components/globals/headers/nav-menu.tsx
+++ src/components/globals/headers/nav-menu.tsx
@@ -1,35 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { NavMenuItemsProps, NavMenuProps } from "@/interfaces/components/globals/headers/nav-menu.interface";
+import { NavLink, useLocation } from "react-router-dom";
 
-export const NavMenu = () => {
-	return <nav className="flex mt-[0.20rem] justify-center items-center text-sm text-muted-foreground">
-		<ul className="flex space-x-4">
-			<li
-				className={
-					location.pathname.includes("/setoran")
-						? "text-foreground"
-						: ""
-				}
-			>
-				<NavLink to="/mahasiswa/dashboard/setoran-hafalan">Setoran Hafalan</NavLink>
-			</li>
-			<li
-				className={
-					location.pathname.includes("/kerja-praktek")
-						? "text-foreground"
-						: ""
-				}
-			>
-				<NavLink to="/mahasiswa/dashboard/kerja-praktek">Kerja Praktek</NavLink>
-			</li>
-			<li
-				className={
-					location.pathname.includes("/seminar")
-						? "text-foreground"
-						: ""
-				}
-			>
-				<NavLink to="/mahasiswa/dashboard/seminar">Seminar</NavLink>
-			</li>
-		</ul>
-	</nav>;
+export const NavMenu = ({ navMenu }: NavMenuProps) => {
+
+	const location = useLocation();
+
+	return (
+		<nav className="flex mt-[0.20rem] justify-center items-center text-sm text-muted-foreground">
+			<ul className="flex space-x-5">
+				{navMenu.map((item: NavMenuItemsProps) => {
+					return (
+						<li key={item.label}
+							className={
+								(location.hash === (item.location_target.replace("/", ""))) ? "text-foreground" : "hover:text-foreground/75"
+							}
+						>
+							<NavLink to={item.location_target}>
+								{item.label}
+							</NavLink>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
 };

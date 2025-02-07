636246bc67083d143f8ad6cbaf3a32cf26601e5b
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-08 00:01:40 +0700

    ⚙ chore: add nav header n oidc client action integrated with kc popup on screen

--- src/pages/publics/landing.page.tsx
+++ src/pages/publics/landing.page.tsx
@@ -1,17 +1,19 @@
 import HeaderLandingPageComponent from "@/components/globals/headers/header-landing-page";
+import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
 
 const LandingPage = () => {
-
 	const auth = useAuth();
+	useEffect(() => {
+		console.log(auth);
+	}, [auth])
 	const handleKeycloakAuth = () => {
-		return auth.isAuthenticated ? auth.signoutRedirect() : void auth.signinRedirect();
+		return auth.isAuthenticated ? auth.signoutSilent() : void auth.signinPopup();
 	}
 
 	return (
 		<div className="w-screen h-screen">
 			<HeaderLandingPageComponent isAuthenticated={auth.isAuthenticated} onContinueWithKeycloakClicked={handleKeycloakAuth}/>
-			<p>gagagadgd</p>
 		</div>
 	);
 };

c1c4b174e16dd048592c9d22450a588df554e8ce
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-09 23:00:29 +0700

    🔥 feat: add log-out action n fallback avatar

--- src/components/globals/sidebars/side-bar-footer.tsx
+++ src/components/globals/sidebars/side-bar-footer.tsx
@@ -1,9 +1,4 @@
-import {
-	Bell,
-	ChevronsUpDown,
-	Edit,
-	LogOut,
-} from "lucide-react";
+import { Bell, ChevronsUpDown, Edit, LogOut } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -20,18 +15,22 @@ import {
 	SidebarMenuItem,
 	useSidebar,
 } from "@/components/ui/sidebar";
+import { UserProfileProps } from "@/interfaces/components/globals/sidebars/app-sidebar.interface";
+import { useAuth } from "react-oidc-context";
+import {
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-export function SideBarFooter({
-	user,
-}: {
-	user: {
-		name: string;
-		email: string;
-		avatar: string;
-	};
-}) {
+export function SideBarFooter({ name, email, avatar }: UserProfileProps) {
+	const auth = useAuth();
 	const { isMobile } = useSidebar();
-
 	return (
 		<SidebarMenu>
 			<SidebarMenuItem>
@@ -42,13 +41,19 @@ export function SideBarFooter({
 							className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
 						>
 							<Avatar className="h-8 w-8 rounded-lg">
-								<AvatarImage src={user.avatar} alt={user.name} />
-								{/* GET USER 2 WORD FALLBACK NAME BY USER.NAME */}
-								<AvatarFallback className="rounded-lg">{ user.name.split(" ").slice(0, 2).map((word) => word.charAt(0)).join("")}</AvatarFallback>
+								<AvatarImage src={avatar} alt={name} />
+								{/* GET USER 2 WORD FALLBACK NAME BY NAME */}
+								<AvatarFallback className="rounded-lg">
+									{name
+										.split(" ")
+										.slice(0, 2)
+										.map((word) => word.charAt(0))
+										.join("")}
+								</AvatarFallback>
 							</Avatar>
 							<div className="grid flex-1 text-left text-sm leading-tight">
-								<span className="truncate font-semibold">{user.name}</span>
-								<span className="truncate text-xs">{user.email}</span>
+								<span className="truncate font-semibold">{name}</span>
+								<span className="truncate text-xs">{email}</span>
 							</div>
 							<ChevronsUpDown className="ml-auto size-4" />
 						</SidebarMenuButton>
@@ -62,12 +67,19 @@ export function SideBarFooter({
 						<DropdownMenuLabel className="p-0 font-normal">
 							<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 								<Avatar className="h-8 w-8 rounded-lg">
-									<AvatarImage src={user.avatar} alt={user.name} />
-									<AvatarFallback className="rounded-lg">CN</AvatarFallback>
+									<AvatarImage src={avatar} alt={name} />
+									{/* GET USER 2 WORD FALLBACK NAME BY NAME */}
+									<AvatarFallback className="rounded-lg">
+										{name
+											.split(" ")
+											.slice(0, 2)
+											.map((word) => word.charAt(0))
+											.join("")}
+									</AvatarFallback>
 								</Avatar>
 								<div className="grid flex-1 text-left text-sm leading-tight">
-									<span className="truncate font-semibold">{user.name}</span>
-									<span className="truncate text-xs">{user.email}</span>
+									<span className="truncate font-semibold">{name}</span>
+									<span className="truncate text-xs">{email}</span>
 								</div>
 							</div>
 						</DropdownMenuLabel>
@@ -82,10 +94,30 @@ export function SideBarFooter({
 							Perbarui Profil
 						</DropdownMenuItem>
 						<DropdownMenuSeparator />
-						<DropdownMenuItem className="cursor-pointer">
+						<AlertDialogTrigger className="cursor-pointer hover:bg-muted w-full relative flex select-none items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&>svg]:size-4 [&>svg]:shrink-0">
 							<LogOut color="#dc2626" />
-							<span className="text-red-600">Log out</span>
-						</DropdownMenuItem>
+							<span className="text-red-600 font-semibold">Keluar</span>
+						</AlertDialogTrigger>
+						<AlertDialogContent>
+							<AlertDialogHeader>
+								<AlertDialogTitle>
+									Apakah kamu yakin mau keluar?
+								</AlertDialogTitle>
+								<AlertDialogDescription>
+									Jika Anda yakin ingin keluar, sesi anda untuk akun ini akan
+									terhapus dan anda akan diarahkan kembali ke halaman awal.
+								</AlertDialogDescription>
+							</AlertDialogHeader>
+							<AlertDialogFooter>
+								<AlertDialogCancel>Gajadi deh</AlertDialogCancel>
+								<AlertDialogAction
+									className="bg-destructive text-destructive-foreground hover:bg-destructive/85"
+									onClick={() => auth.signoutRedirect()}
+								>
+									Iya, saya yakin
+								</AlertDialogAction>
+							</AlertDialogFooter>
+						</AlertDialogContent>
 					</DropdownMenuContent>
 				</DropdownMenu>
 			</SidebarMenuItem>

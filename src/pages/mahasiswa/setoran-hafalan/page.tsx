812e95dc5c845f099a18eb447a5e043daccc5f82
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-09 21:48:58 +0700

    🔥 feat: separate dashboard template being global layout component

--- src/pages/mahasiswa/setoran-hafalan/page.tsx
+++ src/pages/mahasiswa/setoran-hafalan/page.tsx
@@ -1,102 +1,29 @@
-import { AppSidebar } from "@/components/globals/sidebars/app-sidebar";
-import { ModeToggle } from "@/components/themes/mode-toggle";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
-import { Separator } from "@/components/ui/separator";
-import {
-	SidebarInset,
-	SidebarProvider,
-	SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { capitalizeFirstLetter } from "@/helpers/global.helper";
-import { useEffect, useState } from "react";
-
-import { NavLink, useLocation } from "react-router-dom";
+import DashboardLayout from "@/components/globals/layouts/dashboard-layout";
 
 export default function MahasiswaDashboardPage() {
-	const location = useLocation();
-	const [breadcrumbs, setBreadcrumbs] = useState<
-		{ name: string; link: string }[]
-	>([]);
-
-	useEffect(() => {
-		const pathParts = location.pathname.split("/");
-
-		// hanya ambil data ke index ke 2 dst
-		const newPathParts = pathParts.splice(2);
-
-		setBreadcrumbs(
-			newPathParts.map((part, index) => ({
-				name: part.replace("-", " "),
-				link: `/${pathParts.slice(0, index + 1).join("/")}`,
-			}))
-		);
-	}, []);
-
 	return (
 		<>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset>
-					<header className="flex h-16 shrink-0 items-center justify-between pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-						<div className="flex items-center gap-2 px-4">
-							<SidebarTrigger className="-ml-1" />
-							<Separator orientation="vertical" className="mr-2 h-4" />
-							<span className="text-base font-semibold block md:hidden">
-								dashboard<span className="italic font-medium">.tif-usr</span>
-							</span>
-							<Breadcrumb className="hidden md:block">
-								<BreadcrumbList>
-									<NavLink
-										to={"/"}
-										className={"hover:text-foreground/75"}
-									>
-										Beranda
-									</NavLink>								
-									<BreadcrumbSeparator className="hidden md:block" />
-									{breadcrumbs.map((breadcrumb, index) => (
-										<BreadcrumbItem key={index}>
-											{index === breadcrumbs.length - 1 ? (
-												<BreadcrumbPage>
-													{capitalizeFirstLetter(breadcrumb.name)}
-												</BreadcrumbPage>
-											) : (
-												<NavLink
-													to={index === 0 ? "" : breadcrumb.link}
-													className={
-														index === 0
-															? "hover:text-muted-foreground hover:cursor-not-allowed"
-															: "hover:text-foreground/75"
-													}
-												>
-													{capitalizeFirstLetter(breadcrumb.name)}
-												</NavLink>
-											)}
-											{index < breadcrumbs.length - 1 && (
-												<BreadcrumbSeparator className="hidden md:block" />
-											)}
-										</BreadcrumbItem>
-									))}
-								</BreadcrumbList>
-							</Breadcrumb>
-						</div>
-						<ModeToggle />
-					</header>
-					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-						<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-							<div className="aspect-video rounded-xl bg-muted/50" />
-							<div className="aspect-video rounded-xl bg-muted/50" />
-							<div className="aspect-video rounded-xl bg-muted/50" />
-						</div>
-						<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-					</div>
-				</SidebarInset>
-			</SidebarProvider>
+			<DashboardLayout>
+				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+					<div className="aspect-video rounded-xl bg-muted/50" />
+					<div className="aspect-video rounded-xl bg-muted/50" />
+					<div className="aspect-video rounded-xl bg-muted/50" />
+				</div>
+				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+					<div className="aspect-video rounded-xl bg-muted/50" />
+					<div className="aspect-video rounded-xl bg-muted/50" />
+					<div className="aspect-video rounded-xl bg-muted/50" />
+				</div>
+				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
+					<div className="aspect-video rounded-xl bg-muted/50" />
+					<div className="aspect-video rounded-xl bg-muted/50" />
+					<div className="aspect-video rounded-xl bg-muted/50" />
+				</div>
+				<div className="grid auto-rows-min gap-4 md:grid-cols-3"></div>
+				<div className="aspect-video rounded-xl bg-muted/50" />
+				<div className="aspect-video rounded-xl bg-muted/50" />
+				<div className="aspect-video rounded-xl bg-muted/50" />
+			</DashboardLayout>
 		</>
 	);
 }

25a4350e160190edc0ccadc4c43c508b1aa23343
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-09 23:00:59 +0700

    🔥 feat: add super dynamically menus generator based on user role

--- src/components/globals/sidebars/app-sidebar.tsx
+++ src/components/globals/sidebars/app-sidebar.tsx
@@ -12,29 +12,58 @@ import { SideBarFooter } from "./side-bar-footer";
 import { SideBarNavMenu } from "./side-bar-nav-menu";
 import { SideBarNavMenuItems } from "@/constants/components/globals/sidebars/side-bar-nav-menu-item.constant";
 import { Separator } from "@/components/ui/separator";
-
-// This is sample data.
-const data = {
-	user: {
-		name: "M. Farhan Aulia Pratama",
-		email: "12250113521@students.uin-suska.ac.id",
-		avatar: "/avatars/shadcn.jpg",
-	},
-};
+import { useAuth } from "react-oidc-context";
+import { getRoles } from "@/helpers/auth.helper";
+import { capitalizeFirstLetter } from "@/helpers/global.helper";
+import { UserProfileProps } from "@/interfaces/components/globals/sidebars/app-sidebar.interface";
+import { SideBarNavMenuGroupProps } from "@/interfaces/components/globals/sidebars/side-bar-nav-menu.interface";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+	const [user, setUser] = React.useState<UserProfileProps>({
+		name: "Anonymous User",
+		email: "unknown@unknown.com",
+		avatar: "/avatars/shadcn.jpg",
+	});
+	const [roles, setRoles] = React.useState<string[]>([]);
+	const auth = useAuth();
+
+	React.useEffect(() => {
+		setUser({
+			name: auth.user?.profile.name || "Anonymous User",
+			email: auth.user?.profile.email || "unknown@unknown.com",
+			avatar: auth.user?.profile.picture || "/avatars/shadcn.jpg",
+		});
+		const token = auth.user?.access_token;
+		if (token) setRoles(getRoles({ token }));
+	}, []);
+
+	const [roleBasedSideBarNavMenuItems, setRoleBasedSideBarNavMenuItems] =
+		React.useState<SideBarNavMenuGroupProps[]>([]);
+	React.useEffect(() => {
+		const combined = roles.reduce((acc: SideBarNavMenuGroupProps[], role) => {
+			if (SideBarNavMenuItems[role]) {
+				return acc.concat(SideBarNavMenuItems[role]);
+			}
+			return acc;
+		}, []);
+		setRoleBasedSideBarNavMenuItems(combined);
+	}, [roles]);
+
 	return (
 		<Sidebar collapsible="icon" {...props}>
 			<SidebarHeader className="mt-1.5">
-				<SideBarHeader role="Mahasiswa" />
+				<SideBarHeader
+					role={capitalizeFirstLetter(
+						roles.slice(0, -3).join(" & ") || "No Role Specified"
+					)}
+				/>
 			</SidebarHeader>
 			<Separator orientation="horizontal" className="px-4" />
 			<SidebarContent className="gap-0">
-				<SideBarNavMenu sideBarNavMenuItems={SideBarNavMenuItems.mahasiswa} />
+				<SideBarNavMenu sideBarNavMenuItems={roleBasedSideBarNavMenuItems} />
 			</SidebarContent>
-			
 			<SidebarFooter>
-				<SideBarFooter user={data.user} />
+				<SideBarFooter {...user} />
 			</SidebarFooter>
 			<SidebarRail />
 		</Sidebar>
---
+++ src/interfaces/components/globals/sidebars/app-sidebar.interface.ts
@@ -0,0 +1,5 @@
+export interface UserProfileProps {
+	name: string;
+	email: string;
+	avatar: string;
+}

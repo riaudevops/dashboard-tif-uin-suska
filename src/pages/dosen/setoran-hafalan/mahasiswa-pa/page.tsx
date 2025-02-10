9c4603fae7d422b065bdba8ec9c94561c5879d32
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-10 11:25:51 +0700

    ⚙ chore: changed page based routes for next js standard

---
+++ src/pages/dosen/setoran-hafalan/mahasiswa-pa/page.tsx
@@ -0,0 +1,29 @@
+import DashboardLayout from "@/components/globals/layouts/dashboard-layout";
+
+export default function DosenSetoranHafalanMahasiswaPAPage() {
+    return (
+        <>
+            <DashboardLayout>
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                </div>
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                </div>
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                    <div className="aspect-video rounded-xl bg-muted/50" />
+                </div>
+                <div className="grid auto-rows-min gap-4 md:grid-cols-3"></div>
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+            </DashboardLayout>
+        </>
+    );
+}

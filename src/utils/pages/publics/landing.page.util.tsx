16aea1a85aaaa343d93f7037c1f11c9a050156f2
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-02-10 11:34:28 +0700

    ⚙ chore: changed roles based go to dashboard

--- src/utils/pages/publics/landing.page.util.tsx
+++ src/utils/pages/publics/landing.page.util.tsx
@@ -4,7 +4,7 @@ import { DecodeTokenProps } from "@/interfaces/helpers/auth.interface";
 const handleGoToDashboard = ({ token }: DecodeTokenProps) => {
     const userRoles = getRoles({ token });
     if (userRoles.includes("mahasiswa")) return ("/mahasiswa/setoran-hafalan/statistik");
-    else if (userRoles.includes("dosen")) return ("/dosen/setoran-hafalan/pa");
+    else if (userRoles.includes("dosen")) return ("/dosen/setoran-hafalan/mahasiswa-pa");
     else return ("/");
 };
 

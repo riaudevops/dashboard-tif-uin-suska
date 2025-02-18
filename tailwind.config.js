891fe2d09127b543d3b6b157db17b0e9c347c995
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2025-02-18 09:59:34 +0700

    🔥 feat: update tailwind config, enhance vite config, and upgrade package dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
 	content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
 	theme: {
     	extend: {
+			zIndex: {
+				'60': '60'
+			},
     		borderRadius: {
     			lg: 'var(--radius)',
     			md: 'calc(var(--radius) - 2px)',
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import path from "path"
-import react from "@vitejs/plugin-react-swc"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(() => {
+  // const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-})
+    // server: {
+    //   host: '0.0.0.0',
+    //   port: 5173, // atau port lain yang Anda gunakan
+    // },
+    // server: {
+    //   proxy: {
+    //     "/api": {
+    //       target: env.VITE_API_URL,
+    //       changeOrigin: true,
+    //       secure: false,
+    //       rewrite: (path) => path.replace(/^\/api/, ""),
+    //     },
+    //   }
+    // }
+  };
+});

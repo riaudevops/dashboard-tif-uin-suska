c7101e04fc4d4a755e726d991058617f8c1b186f
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2025-02-18 10:06:45 +0700

    🔥 feat: implement detailed statistics page for mahasiswa setoran hafalan with loading state and bar chart visualization

--- src/pages/mahasiswa/setoran-hafalan/statistik/page.tsx
+++ src/pages/mahasiswa/setoran-hafalan/statistik/page.tsx
@@ -1,29 +1,49 @@
 import DashboardLayout from "@/components/globals/layouts/dashboard-layout";
+import BarChartSetoran from "@/components/mahasiswa/setoran-hafalan/statistik/BarChartSetoranHafalan";
+import apiSetoran from "@/services/api/setoran-hafalan/mahasiswa.service";
+import { useQuery } from "@tanstack/react-query";
+import LoadingComponent from "@/components/globals/loading.tsx";
 
+interface MahasiswaSetoranHafalanStatistikPageProps {
+  label: string;
+  persentase: number;
+  total_wajib_setor: number;
+  total_belum_setor: number;
+  total_sudah_setor: number;
+}
 export default function MahasiswaSetoranHafalanStatistikPage() {
-	return (
-		<>
-			<DashboardLayout>
-				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-					<div className="aspect-video rounded-xl bg-muted/50" />
-					<div className="aspect-video rounded-xl bg-muted/50" />
-					<div className="aspect-video rounded-xl bg-muted/50" />
-				</div>
-				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-					<div className="aspect-video rounded-xl bg-muted/50" />
-					<div className="aspect-video rounded-xl bg-muted/50" />
-					<div className="aspect-video rounded-xl bg-muted/50" />
-				</div>
-				<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-					<div className="aspect-video rounded-xl bg-muted/50" />
-					<div className="aspect-video rounded-xl bg-muted/50" />
-					<div className="aspect-video rounded-xl bg-muted/50" />
-				</div>
-				<div className="grid auto-rows-min gap-4 md:grid-cols-3"></div>
-				<div className="aspect-video rounded-xl bg-muted/50" />
-				<div className="aspect-video rounded-xl bg-muted/50" />
-				<div className="aspect-video rounded-xl bg-muted/50" />
-			</DashboardLayout>
-		</>
-	);
+  const { data: dataRingkasan, isLoading } = useQuery({
+    queryKey: ["setoran-saya"],
+    queryFn: () => apiSetoran.getDataMysetoran().then((data) => data.data.setoran.ringkasan),
+    staleTime: Infinity,
+  });
+  return (
+    <DashboardLayout>
+      <div className="flex flex-col gap-1.5">
+        <div className="text-lg md:text-3xl font-bold select-none -ml-1">
+          🔥 Progress Setoran Hafalanmu...
+        </div>
+        <div className="flex flex-col gap-4">
+          <div className="select-none ml-1 md:text-base text-sm">
+            Berikut stastistik dari setoran hafalan kamu untuk persyaratan
+            akademik di UIN Suska Riau, Semangat terus ya... 💙❤️
+          </div>
+          <div className="flex flex-col gap-4">
+            {isLoading && <LoadingComponent />}
+            {dataRingkasan?.map(
+              (item: MahasiswaSetoranHafalanStatistikPageProps) => (
+                <BarChartSetoran
+                  key={item.label}
+                  label={item.label}
+                  persentase={item.persentase}
+                  wajib_setor={item.total_wajib_setor}
+                  telah_setor={item.total_sudah_setor}
+                />
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 }
--- src/pages/publics/landing.page.tsx
+++ src/pages/publics/landing.page.tsx
@@ -1,6 +1,6 @@
 import HeaderLandingPageComponent from "@/components/globals/headers/header-landing-page";
 import LoadingComponent from "@/components/globals/loading";
-import SplashCursor from "@/components/react-bits/splash-cursor";
+// import SplashCursor from "@/components/react-bits/splash-cursor";
 import { handleGoToDashboard } from "@/utils/pages/publics/landing.page.util";
 import { useEffect, useState } from "react";
 import { useAuth } from "react-oidc-context";
@@ -17,7 +17,7 @@ const LandingPage = () => {
 			{auth.isLoading && (<LoadingComponent className="w-screen h-screen bg-black bg-opacity-60 z-50 absolute" />)}
 			<HeaderLandingPageComponent isAuthenticated={auth.isAuthenticated} onContinueWithKeycloakClicked={handleKeycloakAuth} dashboardURL={dashboardURL} />
 			<div className="w-full h-full">			
-				<SplashCursor DENSITY_DISSIPATION={6} VELOCITY_DISSIPATION={4} CAPTURE_RESOLUTION={8000} />
+				{/* <SplashCursor DENSITY_DISSIPATION={6} VELOCITY_DISSIPATION={4} CAPTURE_RESOLUTION={8000} /> */}
 			</div>
 		</div>
 	);

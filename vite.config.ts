7fb26509c7c09858489abff55ba62d6c5d2297bc
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2025-02-20 16:56:59 +0700

    🔥 feat: enhance Vite configuration to load environment variables and set up API proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig(() => {
-  // const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     resolve: {
@@ -11,19 +11,15 @@ export default defineConfig(() => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    // server: {
-    //   host: '0.0.0.0',
-    //   port: 5173, // atau port lain yang Anda gunakan
-    // },
-    // server: {
-    //   proxy: {
-    //     "/api": {
-    //       target: env.VITE_API_URL,
-    //       changeOrigin: true,
-    //       secure: false,
-    //       rewrite: (path) => path.replace(/^\/api/, ""),
-    //     },
-    //   }
-    // }
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      }
+    }
   };
 });

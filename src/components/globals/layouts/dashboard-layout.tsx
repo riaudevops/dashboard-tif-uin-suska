a1e52484e8d774ed5164ef93d742ab784bb83930
FARHANFADHILA <12250113444@students.uin-suska.ac.id>
2025-02-20 17:55:14 +0700

    🔥feat: refactor DashboardLayout component for improved breadcrumb handling and code readability

--- src/components/globals/layouts/dashboard-layout.tsx
+++ src/components/globals/layouts/dashboard-layout.tsx
@@ -1,17 +1,17 @@
 import { AppSidebar } from "@/components/globals/sidebars/app-sidebar";
 import { ModeToggle } from "@/components/themes/mode-toggle";
 import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import {
-	SidebarInset,
-	SidebarProvider,
-	SidebarTrigger,
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { capitalizeFirstLetter } from "@/helpers/global.helper";
 import { DashboardLayoutProps } from "@/interfaces/components/globals/layouts/dashboard-layout.interface";
@@ -20,80 +20,78 @@ import { useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-	const location = useLocation();
-	const [breadcrumbs, setBreadcrumbs] = useState<
-		{ name: string; link: string }[]
-	>([]);
+  const location = useLocation();
+  const [breadcrumbs, setBreadcrumbs] = useState<
+    { name: string; link: string }[]
+  >([]);
 
-	useEffect(() => {
-		const pathParts = location.pathname.split("/");
+  useEffect(() => {
+    const pathParts = location.pathname.split("/");
+    // hanya ambil data ke index ke 2 dst
+    const newPathParts = pathParts.splice(2);
+    // console.log(newPathParts);
+    console.log(pathParts);
 
-		// hanya ambil data ke index ke 2 dst
-		const newPathParts = pathParts.splice(2);
+    setBreadcrumbs(
+      newPathParts.map((part, index) => ({
+        name: part.replace("-", " "),
+        link: `/dosen/${newPathParts.slice(0, index + 1).join("/")}`,
+      }))
+    );
 
-		setBreadcrumbs(
-			newPathParts.map((part, index) => ({
-				name: part.replace("-", " "),
-				link: `/${pathParts.slice(0, index + 1).join("/")}`,
-			}))
-		);
-	}, []);
+    console.log(`/${pathParts.join("/")}`);
+  }, []);
 
-	return (
-		<>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset>
-					<header className="sticky top-0 bg-background flex h-16 shrink-0 items-center justify-between pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 z-50">
-						<div className="flex items-center gap-2 px-4">
-							<SidebarTrigger className="-ml-1" />
-							<Separator orientation="vertical" className="mr-2 h-4" />
-							<span className="text-base font-semibold block md:hidden">
-								dashboard<span className="italic font-medium">.tif-usr</span>
-							</span>
-							<Breadcrumb className="hidden md:block">
-								<BreadcrumbList>
-									<NavLink
-										to={"/"}
-										className={"hover:text-foreground/75"}
-									>
-										Beranda
-									</NavLink>								
-									<BreadcrumbSeparator className="hidden md:block" />
-									{breadcrumbs.map((breadcrumb, index) => (
-										<BreadcrumbItem key={index}>
-											{index === breadcrumbs.length - 1 ? (
-												<BreadcrumbPage>
-													{capitalizeFirstLetter(breadcrumb.name)}
-												</BreadcrumbPage>
-											) : (
-												<NavLink
-													to={index === 0 ? "" : breadcrumb.link}
-													className={
-														index === 0
-															? "hover:text-muted-foreground hover:cursor-not-allowed"
-															: "hover:text-foreground/75"
-													}
-												>
-													{capitalizeFirstLetter(breadcrumb.name)}
-												</NavLink>
-											)}
-											{index < breadcrumbs.length - 1 && (
-												<BreadcrumbSeparator className="hidden md:block" />
-											)}
-										</BreadcrumbItem>
-									))}
-								</BreadcrumbList>
-							</Breadcrumb>
-						</div>
-						<ModeToggle />
-					</header>
-					
-					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-						{children}
-					</div>
-				</SidebarInset>
-			</SidebarProvider>
-		</>
-	);
+  return (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="sticky top-0 bg-background flex h-16 shrink-0 items-center justify-between pr-4 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 z-50">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <span className="text-base font-semibold block md:hidden">
+                dashboard<span className="italic font-medium">.tif-usr</span>
+              </span>
+              <Breadcrumb className="hidden md:block">
+                <BreadcrumbList>
+                  <NavLink to={"/"} className={"hover:text-foreground/75"}>
+                    Beranda
+                  </NavLink>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  {breadcrumbs.map((breadcrumb, index) => (
+                    <BreadcrumbItem key={index}>
+                      {index === breadcrumbs.length - 1 ? (
+                        <BreadcrumbPage>
+                          {capitalizeFirstLetter(breadcrumb.name)}
+                        </BreadcrumbPage>
+                      ) : (
+                        <NavLink
+                          to={index === 0 ? "" : breadcrumb.link}
+                          className={
+                            index === 0
+                              ? "hover:text-muted-foreground hover:cursor-not-allowed"
+                              : "hover:text-foreground/75"
+                          }
+                        >
+                          {capitalizeFirstLetter(breadcrumb.name)}
+                        </NavLink>
+                      )}
+                      {index < breadcrumbs.length - 1 && (
+                        <BreadcrumbSeparator className="hidden md:block" />
+                      )}
+                    </BreadcrumbItem>
+                  ))}
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+            <ModeToggle />
+          </header>
+
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </>
+  );
 }

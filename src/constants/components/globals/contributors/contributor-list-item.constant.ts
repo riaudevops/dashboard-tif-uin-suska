1f736f6b448042078f36a93f553762dbe6fb0126
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-03-09 12:57:32 +0700

    ✨ feat: add ContributorListItems constant for contributor list component

---
+++ src/constants/components/globals/contributors/contributor-list-item.constant.ts
@@ -0,0 +1,86 @@
+import { ContributorListItemProps } from "@/interfaces/components/globals/contributors/contributor-list-item.interface";
+
+export const ContributorListItems: ContributorListItemProps[] = [
+	{
+		image: `https://previews.dropbox.com/p/thumb/ACkd0WWP_LsXxwxzzVOM15WejtMpXJuYqcBq9FXRekfVQ7xj7q3P1xi-Eq7YLMXdrdCfeaoK63u8hKsFP5y36r6Dnq6IyEhdkNkelDYdXHstFOZwDZK_kEZs1rXetRhra2357FLU3RupselLx83ihRDTxPwXnQUSMBRkgCmnE8NYzX0_SMt6xqPdeiqFAaSsOP-gS7kSbDr4oFWvePOOjkV0Ved1VUYr7CpwKU_YPlAIUrKHNvyTRTIAUyj0d_MmLuPg0LOGkfe7ZxV7azLkbMMmI72BxmqBDunt-8gc-s5ITcoDCqNgHH5kjQvmkwZ5HtqqEsdS7JOrKg-7hT3-2mwR/p.jpeg?is_prewarmed=true`,
+		name: "M. Farhan Aulia Pratama",
+		roles: "BackEnd & DevOps Engineer",
+		socialProfiles: {
+			github: "https://github.com/mfarhanz1",
+			linkedIn: "https://id.linkedin.com/in/m-farhan-aulia-pratama-3a5b1a323",
+			instagram: "https://www.instagram.com/mfarhanz1",
+		},
+	},
+	{
+		image:
+			"https://res.cloudinary.com/ddjyfjtsq/image/upload/v1741464541/gilang_ez2aio.jpg",
+		name: "Gilang Ramadhan Indra",
+		roles: "BackEnd Engineer",
+		socialProfiles: {
+			github: "https://github.com/gindra-o7",
+			linkedIn: "www.linkedin.com/in/gilang-ramadhan-indra",
+			instagram: "https://www.instagram.com/gilanginr_",
+		},
+	},
+	{
+		image:
+			"https://res.cloudinary.com/dkoaxiwdd/image/upload/v1741482800/4-5_jou254.jpg",
+		name: "Abmi Sukma Edri",
+		roles: "UI/UX Designer",
+		socialProfiles: {
+			github: "https://github.com/AbmiSukma",
+			linkedIn: "https://www.linkedin.com/in/abmi-sukma-1948ab250/",
+			instagram: "https://www.instagram.com/abmisukma/",
+		},
+	},
+	{
+		image:
+			"https://res.cloudinary.com/dljiuag9b/image/upload/v1741471350/rafly.jpg",
+		name: "Muhammad Rafly Wirayudha",
+		roles: "FrontEnd Engineer",
+		socialProfiles: {
+			github: "https://github.com/raflywirayudha",
+			linkedIn:
+				"https://www.linkedin.com/in/muhammad-rafly-wirayudha-619653239/",
+			instagram: "https://www.instagram.com/raflywirayudhaa",
+		},
+	},
+	{
+		image:
+			"https://res.cloudinary.com/nabildawami/image/upload/v1741496937/hod4nkhprfjlaf1xzd36.jpg",
+		name: "M. Nabil Dawami",
+		roles: "UI/UX Designer",
+		socialProfiles: {
+			github: "https://github.com/MNabildawami",
+			linkedIn: "https://www.linkedin.com/in/m-nabil-dawami-b55380215/",
+			instagram: "https://www.instagram.com/naabildawami",
+		},
+	},
+	{
+		image:
+			"https://res.cloudinary.com/dbsltir7r/image/upload/v1741497104/c45fccd6-ac7d-4f2e-80f4-b305e526a4b5_uxodiz.png",
+		name: "Muh. Zaki Erbai Syas",
+		roles: "BackEnd Engineer",
+		socialProfiles: {
+			github: "https://github.com/rel1vee",
+			linkedIn:
+				"https://www.linkedin.com/in/muhzakierbaisyas",
+			instagram: "https://www.linkedin.com/in/muhzakierbaisyas/",
+		},
+	},
+	{
+		image: `https://storage.ruanjipos.id/image/gasd`,
+		name: "Farhan Fadhila",
+		roles: "FrontEnd Engineer",
+	},
+	{
+		image: `https://storage.ruanjipos.id/image/gasd`,
+		name: "Ahmad Kurniawan",
+		roles: "FrontEnd Engineer",
+	},
+	{
+		image: `https://storage.ruanjipos.id/image/gasd`,
+		name: "Hafidz Alhadid Rahman",
+		roles: "BackEnd Engineer",
+	},
+];

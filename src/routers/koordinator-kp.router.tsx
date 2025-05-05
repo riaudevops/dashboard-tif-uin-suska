3eac7728efa3d63590517f7224373c8eb7e69f60
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-05 11:08:51 +0700

    feat: Add koordinator router configuration for kerja praktik and seminar pages

---
+++ src/routers/koordinator-kp.router.tsx
@@ -0,0 +1,58 @@
+import KoordinatorKerjaPraktikPermohonanDetailpage from "@/pages/Koordinator/Kerja-Praktik/Permohonan/Detail-Permohonan/page";
+import ProtectedRoute from "./protected.router";
+import KoordinatorKerjaPraktikDailyReportpage from "@/pages/Koordinator/Kerja-Praktik/Daily-Report/page";
+import KoordinatorKerjaPraktikDailyReportDetailpage from "@/pages/Koordinator/Kerja-Praktik/Daily-Report/Detail-Mahasiswa/page";
+import KoordinatorValidasiBerkasPage from "@/pages/Koordinator/seminar-kerja-praktek/validasi-berkas/page";
+import KoordinatorNilaiPage from "@/pages/Koordinator/seminar-kerja-praktek/nilai/page";
+import KoordinatorKerjaPraktikPermohonanpage from "@/pages/Koordinator/Kerja-Praktik/Permohonan/page";
+
+export const koordinatorKPRouter = [
+	{
+		path: "/koordinator-kp/kerja-praktik/permohonan",
+		element: (
+			<ProtectedRoute roles={["koordinator-kp"]}>
+				<KoordinatorKerjaPraktikPermohonanpage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/koordinator-kp/kerja-praktik/permohonan/detail-permohonan",
+		element: (
+			<ProtectedRoute roles={["koordinator-kp"]}>
+				<KoordinatorKerjaPraktikPermohonanDetailpage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/koordinator-kp/kerja-praktik/daily-report",
+		element: (
+			<ProtectedRoute roles={["koordinator-kp"]}>
+				<KoordinatorKerjaPraktikDailyReportpage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/koordinator-kp/kerja-praktik/daily-report/detail-mahasiswa",
+		element: (
+			<ProtectedRoute roles={["koordinator-kp"]}>
+				<KoordinatorKerjaPraktikDailyReportDetailpage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/koordinator-kp/seminar-kp/validasi-berkas",
+		element: (
+			<ProtectedRoute roles={["koordinator-kp"]}>
+				<KoordinatorValidasiBerkasPage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: "/koordinator-kp/seminar-kp/nilai",
+		element: (
+			<ProtectedRoute roles={["koordinator-kp"]}>
+				<KoordinatorNilaiPage />
+			</ProtectedRoute>
+		),
+	},
+]

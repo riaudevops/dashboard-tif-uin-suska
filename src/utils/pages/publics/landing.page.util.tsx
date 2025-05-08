955c2039b47dbd657206b36969b4f7a0f03940b3
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-08 11:17:22 +0700

    feat: update navigation paths from 'setoran-hafalan' to 'murojaah' in handleGoToDashboard function

--- src/utils/pages/publics/landing.page.util.tsx
+++ src/utils/pages/publics/landing.page.util.tsx
@@ -3,8 +3,8 @@ import { DecodeTokenProps } from "@/interfaces/helpers/auth.interface";
 
 const handleGoToDashboard = ({ token }: DecodeTokenProps) => {
     const userRoles = getRoles({ token });
-    if (userRoles.includes("mahasiswa")) return ("/mahasiswa/setoran-hafalan/statistik");
-    else if (userRoles.includes("dosen")) return ("/dosen/setoran-hafalan/mahasiswa-pa");
+    if (userRoles.includes("mahasiswa")) return ("/mahasiswa/murojaah/statistik");
+    else if (userRoles.includes("dosen")) return ("/dosen/murojaah/mahasiswa-pa");
     else return ("/");
 };
 

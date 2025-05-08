a45ae06ad5d85239f0f19bb21781fc77fd7c7998
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-08 11:16:54 +0700

    feat: update terminology from 'murojaah' to 'Muroja'ah' in capitalizeFirstLetter function

--- src/helpers/global.helper.ts
+++ src/helpers/global.helper.ts
@@ -1,5 +1,5 @@
 export const capitalizeFirstLetter = (string: string) => {
-    const newString = string.replace("-kp", " KP").replace("-ta", " TA").replace(" pa", " PA").replace(" kp", " KP")
+    const newString = string.replace("-kp", " KP").replace("-ta", " TA").replace(" pa", " PA").replace(" kp", " KP").replace("murojaah", "Muroja'ah");
     const words = newString.split(" ").map((word) => word.trim());
     for (let i = 0; i < words.length; i++) {
         words[i] = words[i][0].toUpperCase() + words[i].substring(1);

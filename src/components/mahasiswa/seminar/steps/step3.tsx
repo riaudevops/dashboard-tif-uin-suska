358327a0ee7ce7ca4876180edf8b7a8df892a638
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-05-24 15:40:47 +0700

    feat: update Step3 component header with new layout and icon for enhanced visibility

--- src/components/mahasiswa/seminar/steps/step3.tsx
+++ src/components/mahasiswa/seminar/steps/step3.tsx
@@ -3,7 +3,7 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import Stepper from "@/components/mahasiswa/seminar/stepper";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { RefreshCw } from "lucide-react";
+import { LayoutGridIcon, RefreshCw } from "lucide-react";
 import Status from "@/components/mahasiswa/seminar/status";
 import InfoCard from "../informasi-seminar";
 import DocumentCard from "../formulir-dokumen";
@@ -435,9 +435,15 @@ const Step3: FC<Step3Props> = ({ activeStep }) => {
 
   return (
     <div className="space-y-4">
-      <h1 className="text-2xl font-bold mb-8">
-        Validasi Kelengkapan Berkas Seminar Kerja Praktik
-      </h1>
+      <div className="flex mb-5">
+        <span className="bg-white flex justify-center items-center shadow-sm text-gray-800 dark:text-gray-200 dark:bg-gray-900 px-2 py-0.5 rounded-md border border-gray-200 dark:border-gray-700 text-md font-medium tracking-tight">
+          <span
+            className={`inline-block animate-pulse w-3 h-3 rounded-full mr-2 bg-yellow-400`}
+          />
+          <LayoutGridIcon className="w-4 h-4 mr-1.5" />
+          Validasi Kelengkapan Berkas Seminar Kerja Praktik Mahasiswa            
+        </span>
+      </div>
 
       <Stepper activeStep={activeStep} />
 

9849d8e28e02bb10c319f14146deef3ea80c32de
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-06-28 13:53:20 +0700

    fix: update API endpoint for getKartuMurojaahDigital method to use the correct base URL

--- src/services/api/public/setoran-hafalan.service.ts
+++ src/services/api/public/setoran-hafalan.service.ts
@@ -4,7 +4,7 @@ export default class APISetoran {
   public static async getKartuMurojaahDigital({id}: { id: string }) {
     const axios = api();
     const response = await axios.get(
-      `${import.meta.env.VITE_BASE_URL_PUBLIC}/mahasiswa/kartu-murojaah-digital/${id}`,
+      `${import.meta.env.VITE_BASE_URL_PUBLIC_SETORAN_HAFALAN}/hanz/kartu-murojaah-digital/${id}`,
     );
     const data = response.data;
     return data;

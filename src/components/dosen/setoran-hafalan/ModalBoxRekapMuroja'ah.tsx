fda5b1eabf762d67c4263ea8f1ad137082f5ae66
MFarhanZ1 <farhanaulia.p@gmail.com>
2025-07-06 13:51:20 +0700

    ✨feat: Integrate MonthYearPicker component and update button functionality in ModalBoxRekapMurojaah

--- src/components/dosen/setoran-hafalan/ModalBoxRekapMuroja'ah.tsx
+++ src/components/dosen/setoran-hafalan/ModalBoxRekapMuroja'ah.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { ClipboardListIcon, Loader2, X } from "lucide-react";
-import { useEffect, useMemo, useState } from "react";
+import { ClipboardListIcon, Loader2, PrinterIcon, X } from "lucide-react";
+import { useEffect, useState } from "react";
+import { MonthYearPicker } from "./MonthYearPicker";
 
 function ModalBoxRekapMurojaah({
 	isOpen,
@@ -11,65 +12,46 @@ function ModalBoxRekapMurojaah({
 }: {
 	isOpen: boolean;
 	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-	handleButtonNext: (bulan: string, tahun: string) => void;
-	handleButtonNextMobile: (bulan: string, tahun: string) => void;
+	handleButtonNext: (bulan: number, tahun: number) => void;
+	handleButtonNextMobile: (bulan: number, tahun: number) => void;
 	buttonLoading?: boolean;
 }) {
-	const [selectedMonth, setSelectedMonth] = useState<string>("");
-	const [selectedYear, setSelectedYear] = useState<string>("");
+	// dapatkan bulan dan tahun saat ini
+	const currentMonth = new Date().getMonth() + 1;
+	const currentYear = new Date().getFullYear();
 
-	const months = [
-		{ value: "1", label: "Januari" },
-		{ value: "2", label: "Februari" },
-		{ value: "3", label: "Maret" },
-		{ value: "4", label: "April" },
-		{ value: "5", label: "Mei" },
-		{ value: "6", label: "Juni" },
-		{ value: "7", label: "Juli" },
-		{ value: "8", label: "Agustus" },
-		{ value: "9", label: "September" },
-		{ value: "10", label: "Oktober" },
-		{ value: "11", label: "November" },
-		{ value: "12", label: "Desember" },
-	];
-
-	const years = useMemo(() => {
-		const currentYear = new Date().getFullYear();
-		const yearOptions = [];
-		for (let i = 0; i < 5; i++) {
-			yearOptions.push(String(currentYear - i));
-		}
-		return yearOptions;
-	}, []);
+	const [monthYear, setMonthYear] = useState<{ bulan: number; tahun: number }>({
+		bulan: currentMonth,
+		tahun: currentYear,
+	});
 
 	const handleNext = () => {
-		if (!selectedMonth || !selectedYear) {
+		if (!monthYear.bulan || !monthYear.tahun) {
 			// Menggunakan console.error agar lebih terlihat saat debugging
 			console.error("Bulan dan tahun wajib dipilih.");
 			return;
 		}
 
-		console.log(selectedMonth, selectedYear);
+		console.log(monthYear.bulan, monthYear.tahun);
 		// Panggil fungsi handleButtonNext dengan bulan dan tahun yang dipilih
-		handleButtonNext(selectedMonth, selectedYear);
+		handleButtonNext(monthYear.bulan, monthYear.tahun);
 	};
 	const handleNextButton = () => {
-		if (!selectedMonth || !selectedYear) {
+		if (!monthYear.bulan || !monthYear.tahun) {
 			// Menggunakan console.error agar lebih terlihat saat debugging
 			console.error("Bulan dan tahun wajib dipilih.");
 			return;
 		}
 
-		console.log(selectedMonth, selectedYear);
+		console.log(monthYear.bulan, monthYear.tahun);
 		// Panggil fungsi handleButtonNext dengan bulan dan tahun yang dipilih
-		handleButtonNextMobile(selectedMonth, selectedYear);
+		handleButtonNextMobile(monthYear.bulan, monthYear.tahun);
 	};
 	useEffect(() => {
 		// Efek ini akan berjalan setiap kali nilai `isOpen` berubah.
 		if (isOpen) {
 			// Jika modal dibuka, reset state bulan dan tahun.
-			setSelectedMonth("");
-			setSelectedYear("");
+			setMonthYear({ bulan: currentMonth, tahun: currentYear });
 		}
 	}, [isOpen]);
 	return (
@@ -82,7 +64,7 @@ function ModalBoxRekapMurojaah({
 			>
 				<div
 					onClick={(e) => e.stopPropagation()}
-					className="bg-gradient-to-br from-red-100/50 via-violet-100/50 to-pink-100/50 dark:from-black dark:via-violet-900/20 dark:to-black rounded-2xl shadow-2xl w-full max-w-xl relative animate-in fade-in-0 zoom-in-95"
+					className="bg-gradient-to-br from-red-100/50 via-violet-100/50 to-pink-100/50 dark:from-black dark:via-violet-900/20 dark:to-black rounded-2xl shadow-2xl w-full max-w-md relative animate-in fade-in-0 zoom-in-95"
 				>
 					<button
 						onClick={() => setIsOpen(false)}
@@ -92,7 +74,7 @@ function ModalBoxRekapMurojaah({
 					</button>
 
 					<div className="rounded-2xl shadow-md md:p-8 p-3 border border-foreground/10">
-						<div className="flex items-center mb-8 group/header">
+						<div className="flex items-center mb-6 group/header">
 							<div className="relative">
 								<div className="absolute inset-0 bg-gradient-to-r from-green-500 to-violet-600 rounded-2xl blur-lg opacity-30 group-hover/header:opacity-50 transition-opacity duration-300"></div>
 								<div className="relative flex items-center justify-center w-12 h-12 bg-gradient-to-br from-red-500 via-violet-600 to-green-600 rounded-2xl shadow-lg mr-4 group-hover/header:scale-110 transition-transform duration-300">
@@ -112,58 +94,30 @@ function ModalBoxRekapMurojaah({
 							</div>
 						</div>
 
-						<div className="rounded-2xl shadow-md md:p-8 p-3 border border-foreground/10">
-							<div className="flex flex-col gap-4 py-2">
-								{/* Grid responsif: 1 kolom di mobile, 2 kolom di layar lebih besar */}
-								<div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-									<select
-										className="border border-gray-300 bg-background rounded-md py-2 px-1.5 text-foreground"
-										onChange={(e) => setSelectedMonth(e.target.value)}
-										value={selectedMonth}
-									>
-										<option disabled value="">
-											Pilih Bulan
-										</option>
-										{months.map((month) => (
-											<option key={month.value} value={month.value}>
-												{month.label}
-											</option>
-										))}
-									</select>
-
-									<select
-										className="border border-gray-300 bg-background rounded-md py-2 px-1.5 text-foreground"
-										onChange={(e) => setSelectedYear(e.target.value)}
-										value={selectedYear}
-									>
-										<option disabled value="">
-											Pilih Tahun
-										</option>
-										{years.map((year) => (
-											<option key={year} value={year}>
-												{year}
-											</option>
-										))}
-									</select>
-								</div>
-
-								<Button
-									onClick={handleNext}
-									disabled={!selectedMonth || !selectedYear}
-									className="w-full bg-green-600 hover:bg-green-700 text-white dark:bg-green-500 hidden md:flex"
-								>
-									{buttonLoading && <Loader2 className="mr-1 animate-spin" />}
-									Selanjutnya
-								</Button>
-								<Button
-									onClick={handleNextButton}
-									disabled={!selectedMonth || !selectedYear}
-									className="w-full bg-green-600 hover:bg-green-700 text-white dark:bg-green-500 md:hidden"
-								>
-									{buttonLoading && <Loader2 className="mr-1 animate-spin" />}
-									Selanjutnya
-								</Button>
+						<div className="flex flex-col gap-4 py-2 -mb-2">
+							{/* Grid responsif: 1 kolom di mobile, 2 kolom di layar lebih besar */}
+							<div className="w-full">
+								<MonthYearPicker value={monthYear} onChange={setMonthYear} />
 							</div>
+
+							<Button
+								onClick={handleNext}
+								disabled={!monthYear.bulan || !monthYear.tahun || buttonLoading}
+								className="w-full bg-green-600 hover:bg-green-700 text-white dark:bg-green-500 hidden md:flex"
+							>
+								{buttonLoading && <Loader2 className="mr-1 animate-spin" />}
+								<PrinterIcon />
+								Cetak Kartu Rekapan
+							</Button>
+							<Button
+								onClick={handleNextButton}
+								disabled={!monthYear.bulan || !monthYear.tahun || buttonLoading}
+								className="w-full bg-green-600 hover:bg-green-700 text-white dark:bg-green-500 md:hidden"
+							>
+								{buttonLoading && <Loader2 className="mr-1 animate-spin" />}
+								<PrinterIcon />
+								Cetak Kartu Rekapan
+							</Button>
 						</div>
 					</div>
 				</div>
